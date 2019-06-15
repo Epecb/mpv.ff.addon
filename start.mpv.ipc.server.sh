19fdfc59f2b3f8dc37aa5e0e7570b016d9bff7f6
Epecb <788135@gmail.com>
2019-06-15 22:32:05 +0300

    Fix and clean script
        Fix: change shebang
        Fix: close script when quit MPV (BUG: wait any command from ff)
        Clear: remove commented line for debugging.
    
    Signed-off-by: Epecb <788135@gmail.com>

--- start.mpv.ipc.server.sh
+++ start.mpv.ipc.server.sh
@@ -1,8 +1,9 @@
-#!/bin/sh
+#!/usr/bin/env sh
+
 
 mpv --force-window=immediate --osd-level=3 --speed=2.42 --ytdl-format=best --idle --input-ipc-server=/tmp/mpvsocket &
+sleep 1
 
-while true; do
+while [ $(lsof -t /tmp/mpvsocket) ]; do
 	echo "HTTP/1.0 200 OK" | nc -l -p 8888 | sed -un '/^POST/,/^\r/!p;a \\n' | socat - /tmp/mpvsocket
-	# echo "HTTP/1.0 200 OK" | nc -l -p 8888 | sed -un '/^POST/,/^\r/!p;a \\n'
 done
