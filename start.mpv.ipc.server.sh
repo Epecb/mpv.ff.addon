e84d820b14369112a10a5388e591563b90ad460d
Epecb <788135@gmail.com>
2021-03-27 21:42:53 +0300

    Format and Log
        Add log file in memory /dev/shm
        Fix mpv ytdl video format
    
    Signed-off-by: Epecb <788135@gmail.com>

--- start.mpv.ipc.server.sh
+++ start.mpv.ipc.server.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env sh
 
 
-mpv --force-window=immediate --osd-level=3 --speed=2.42 --ytdl-format=best --idle --input-ipc-server=/tmp/mpvsocket &
+mpv --force-window=immediate --osd-level=3 --speed=2.42 --ytdl-format='best[height<=720]/bestvideo[height<=720]+bestaudio' --idle --input-ipc-server=/tmp/mpvsocket &
 sleep 1
 
-while [ $(lsof -t /tmp/mpvsocket) ]; do
-	echo "HTTP/1.0 200 OK" | nc -l -p 8888 | sed -un '/^POST/,/^\r/!p;a \\n' | socat - /tmp/mpvsocket
+while [ "$(lsof -t /tmp/mpvsocket)" ]; do
+	echo "HTTP/1.0 200 OK" | nc -l -p 8888 | sed -un '/^POST/,/^\r/!p;$a \\' | tee -a /dev/shm/mpv.log.url.txt | socat - /tmp/mpvsocket
 done
